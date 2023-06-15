005d83db99345ae4d4f0175c9271fc694acc00cb
ad <rakesh.singh470@gmail.com>
2023-06-15 17:18:49 +0530

    test
    Signed-off-by: ad <rakesh.singh470@gmail.com>

---
+++ hi/src/com123/Test.java
@@ -0,0 +1,13 @@
+package com123;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1q");
+	}
+
+}
